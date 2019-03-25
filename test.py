c0977fe834df1b1b4d09af9834c23dea7a9dde1e
Tzvi <tzdovi@gmail.com>
2019-03-25 21:10:57 +0200

    test modified

--- test.py
+++ test.py
@@ -1,19 +1,18 @@
 import main as mn
 from whatsapp_client import main as client
-
-import threading
+from threading import Thread
 import sys
 import random
 import string
 
 
-def gen_test(num_lines=5):
-	f = open("test.txt", "w")
+def gen_input(output="test.txt", num_lines=5):
+	f = open(output, "w")
 	for i in range(num_lines):
 		choice = random.randint(0, 100)
 		if choice < 80:
 			print("s", file=f)
-			message = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30))
+			message = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(30))
 			print(message, file=f)
 		elif choice < 90:
 			print("r", file=f)
@@ -24,22 +23,16 @@ def gen_test(num_lines=5):
 	f.close()
 
 
-class Tee(object):
-	def __init__(self, input_handle, output_handle):
-		self.input = input_handle
-		self.output = output_handle
-
-	def readline(self):
-		result = self.input.readline()
-		self.output.write(result)
-
-		return result
-
-
 if __name__ == '__main__':
-	gen_test(50)
-	f = open("test.txt")
-	if not sys.stdin.isatty():
-		sys.stdin = Tee(input_handle=f, output_handle=sys.stdout)
-		client()
-	sys.stdin = sys.__stdin__
+	# servers = Thread(target=mn.run_servers())
+	# servers.start()
+	input_file = "input.txt"
+	output_file = "output.txt"
+	gen_input(input_file, 50)
+	# if not sys.stdin.isatty():
+	# 	sys.stdin = open(input_file)
+	# 	sys.stdout = open(output_file, "w")
+	# 	client()
+	# 	sys.stdin = sys.__stdin__
+	# 	sys.stdout = sys.__stdout__
+	# servers.join(50)
